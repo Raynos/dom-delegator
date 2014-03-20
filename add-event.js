8bf6c52592f9ebd53656f44c165566483ca38c72
Raynos (Jake Verbaten) <raynos2@gmail.com>
2014-03-20 02:20:12 -0700

    support multiple handlers

--- add-event.js
+++ add-event.js
@@ -5,8 +5,28 @@ module.exports = addEvent
 
 function addEvent(id, target, type, fn) {
     var events = getField(target, id)
-    events[type] = typeof fn === "function" ?
+
+    var handler = typeof fn === "function" ?
         { handleEvent: fn } : fn
+    var currentHandler = events[type]
+
+    if (currentHandler) {
+        events[type] = new MultiHandler(currentHandler, handler)
+    } else {
+        events[type] = handler
+    }
 
     setField(target, id, events)
+}
+
+function MultiHandler(multi, handler) {
+    var handles = multi.handles || [multi]
+
+    this.handles = handles.concat([handler])
+}
+
+MultiHandler.prototype.handleEvent = function (ev) {
+    this.handles.forEach(function (handler) {
+        handler.handleEvent(ev)
+    })
 }
