4599121a49357667ebc254940da511d7ea473f5f
Raynos <raynos2@gmail.com>
2014-03-30 17:15:37 -0700

    comment get-listener

--- get-listener.js
+++ get-listener.js
@@ -3,11 +3,13 @@ var DataSet = require("data-set")
 module.exports = getListener
 
 function getListener(surface, id, target, type) {
+    // terminate recursion if parent is `null`
     if (target === null) {
         return null
     }
 
     var ds = DataSet(target)
+    // fetch list of handler fns for this event
     var handlers = getHandlers(ds[type] || {}, ds.event || {}, id)
 
     if (!handlers) {
@@ -19,9 +21,12 @@ function getListener(surface, id, target, type) {
 }
 
 function getHandlers(events, allEvents, id) {
+    // support { handleEvent: Function, id: String }
+    // and Object<id: String, Function | { handleEvent: Function }>
     var handler = events.id === id ? events : events[id]
     var allHandler = allEvents.id === id ? allEvents : allEvents[id]
 
+    // return null if nothing else flatten into one array
     return !handler && !allHandler ?
         null : [].concat(handler || [], allHandler || [])
 }
