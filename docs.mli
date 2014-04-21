e2f6a59c9b62c31710ab70b860006094347af473
Raynos <raynos2@gmail.com>
2014-04-20 23:50:20 -0700

    updated docs.mli

--- docs.mli
+++ docs.mli
@@ -1,5 +1,11 @@
-type EventHandler := Function | {
-    handleEvent: Function
+type ProxyEvent := DOMEvent & {
+    _rawEvent: DOMEvent,
+    currentTarget: DOMNode,
+    preventDefault: () => void
+}
+
+type EventHandler := Function<ProxyEvent> | {
+    handleEvent: Function<ProxyEvent>
 }
 
 type Delegator := {
@@ -7,18 +13,24 @@ type Delegator := {
     listenTo: (eventName: String) => void,
     unlistenTo: (eventName: String) => void,
     addEventListener: (DOMNode, String, EventHandler),
-    removeEventListener: (DOMNode, String, EventHandler)
+    removeEventListener: (DOMNode, String, EventHandler),
+    addGlobalEventListener: (String, EventHandler),
+    removeGlobalEventListener: (String, EventHandler)
 }
 
 dom-delegator := (opts?: {
     defaultEvents?: Boolean
 }) => Delegator
 
+dom-delegator/dom-delegator := () => Delegator
+
 dom-delegator/add-event := (
     target: DOMNode,
     type: String,
     fn: EventHandler
 ) => void
+
+dom-delegator/proxy-event := (DOMEvent) => ProxyEvent
 
 dom-delegator/remove-event := (
     target: DOMNode,
